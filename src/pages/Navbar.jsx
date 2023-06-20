cc0baac24db4a9c23b35f54ee6e8df05d1c5c7fe
David Omsiakin <davidomisakin4good@gmail.com>
2023-06-21 00:09:04 +0100

    done

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import logo from '../images/mls_logo.png'
 import '../css/style.css'
+import { NavLink } from "react-router-dom";
 import image from '../images/hamburger-menu.svg'
 
 import { Fa } from 'react-icons/fa';
@@ -21,7 +22,13 @@ function Navbar() {
           isNavExpanded ? "nav-list expanded" : "nav-list"
         }>
             <ul>
-                <li><a href={'foreclosures'}>Foreclosures</a></li>
+                <li><a href={'/foreclosures'}>Foreclosures</a></li>
+                <NavLink
+                  to='/foreclosures'
+                  className="nav-link active text-dark"
+                  aria-current="page"
+                  href="#"
+                ></NavLink>
                 <li><a href={'https://www.newhomesource.com/?refer=mlscom'} target='_blank'>New Homes</a></li>
                 <li><a href={'https://www.topagentsranked.com/signup02?utm_source=mlsdotcom&utm_campaign=textlink'} target='_blank'>Find an Agent</a></li>
                 <li><a href={''}>Real Estate Classes</a></li>
